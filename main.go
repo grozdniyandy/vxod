47902bb88541b5570f6c8ed0f3461a1e89e65cb9
grozdniyandy <143331225+grozdniyandy@users.noreply.github.com>
2023-11-04 15:28:44 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: go run main.go domain1.com domain2.com ...")
+        return
+    }
+
+    domains := os.Args[1:]
+
+    for _, domain := range domains {
+        fmt.Printf("Checking %s...\n", domain)
+        checkForInputFields("http://" + domain)
+        checkForInputFields("https://" + domain)
+    }
+}
+
+func checkForInputFields(url string) {
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode != 200 {
+        log.Fatalf("Failed to fetch %s: %s", url, resp.Status)
+        return
+    }
+
+    doc, err := goquery.NewDocumentFromReader(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    hasInputFields := false
+
+    doc.Find("input").Each(func(i int, s *goquery.Selection) {
+        inputType, exists := s.Attr("type")
+        if !exists || (inputType != "hidden" && inputType != "submit" && inputType != "reset") {
+            hasInputFields = true
+            return
+        }
+    })
+
+    if hasInputFields {
+        fmt.Printf("%s contains input fields.\n", url)
+    } else {
+        fmt.Printf("%s does not contain input fields.\n", url)
+    }
+}
